88d7a6322bd74518df635f3ee1681f90fbfd0541
RhinoLee <picklerhino@gmail.com>
2023-03-13 14:38:42 +0800

    初始化 Spring Boot project

---
+++ springboot-mall/src/test/java/com/rhinolee/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhinolee.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
