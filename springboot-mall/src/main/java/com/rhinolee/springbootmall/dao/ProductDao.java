05c603d812ded7ef6b58f827cf1408530783b8e0
RhinoLee <picklerhino@gmail.com>
2023-03-20 00:18:10 +0800

    商品功能 - 查詢商品列表（加上查詢條件）

--- springboot-mall/src/main/java/com/rhinolee/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/rhinolee/springbootmall/dao/ProductDao.java
@@ -1,5 +1,6 @@
 package com.rhinolee.springbootmall.dao;
 
+import com.rhinolee.springbootmall.constant.ProductCategory;
 import com.rhinolee.springbootmall.dto.ProductRequest;
 import com.rhinolee.springbootmall.model.Product;
 
@@ -7,7 +8,7 @@ import java.util.List;
 
 public interface ProductDao {
 
-    List<Product> getProducts();
+    List<Product> getProducts(ProductCategory category, String search);
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- springboot-mall/src/main/java/com/rhinolee/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/rhinolee/springbootmall/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.rhinolee.springbootmall.service;
 
+import com.rhinolee.springbootmall.constant.ProductCategory;
 import com.rhinolee.springbootmall.dto.ProductRequest;
 import com.rhinolee.springbootmall.model.Product;
 
@@ -7,7 +8,7 @@ import java.util.List;
 
 public interface ProductService {
 
-    List<Product> getProducts();
+    List<Product> getProducts(ProductCategory category, String search);
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
