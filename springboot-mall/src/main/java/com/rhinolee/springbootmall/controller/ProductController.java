ce93c42c9c1084ff0fc390d1d5d0abcaad7a7f85
RhinoLee <picklerhino@gmail.com>
2023-03-21 15:52:12 +0800

    商品功能 - 商品功能單元測試

--- springboot-mall/src/main/java/com/rhinolee/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/rhinolee/springbootmall/controller/ProductController.java
@@ -61,7 +61,7 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
-    @GetMapping("/product/{productId}")
+    @GetMapping("/products/{productId}")
     public ResponseEntity<Product> getProduct(@PathVariable Integer productId) {
         Product product = productService.getProductById(productId);
 
